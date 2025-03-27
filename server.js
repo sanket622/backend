6e165a836ed93328c5d19938444c86eace15f0e6
sanket622 <sanketkumar0068@gmail.com>
2025-03-27 16:12:32 +0530

    Update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import axios from 'axios'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 const app = express()
